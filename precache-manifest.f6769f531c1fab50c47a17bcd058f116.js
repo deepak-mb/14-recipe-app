b2dd6eb5d28242026138b4b389a3856cdfad5a79
Deepak <deepak25mb@gmail.com>
2019-06-21 11:47:23 +0530

    Updates

---
+++ precache-manifest.f6769f531c1fab50c47a17bcd058f116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a2986c87b20d23529d95d85f4c60fb",
+    "url": "/14-recipe-app/index.html"
+  },
+  {
+    "revision": "b35cda9100e5b58c5832",
+    "url": "/14-recipe-app/static/css/main.7b2f78ad.chunk.css"
+  },
+  {
+    "revision": "d90d6a8623103d0adf11",
+    "url": "/14-recipe-app/static/js/2.a1f1ffa8.chunk.js"
+  },
+  {
+    "revision": "b35cda9100e5b58c5832",
+    "url": "/14-recipe-app/static/js/main.2bc5ff85.chunk.js"
+  },
+  {
+    "revision": "6145d835a437c0bc8e30",
+    "url": "/14-recipe-app/static/js/runtime~main.e4ff8457.js"
+  }
+]);
